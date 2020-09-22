f5217e4eb142c1adca7d4f8e544dede0c7481340
Ed Summers <ehs@pobox.com>
2020-09-22 07:59:30 -0400

    for deploy

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name = 'dedoop',
-    version = '0.0.3',
+    version = '0.0.4',
     author = 'Ed Summers',
     author_email = 'ehs@pobox.com',
     url = 'https://github.com/edsu/dedoop',
     py_modules = ['dedoop',],
     description = 'dedupe files and send them to the cloud',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires = ['apache-libcloud', 'click'],
     setup_requires=['pytest-runner'],
     tests_require = ['pytest', 'python-dotenv'],
